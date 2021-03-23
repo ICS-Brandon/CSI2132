c0de633569ae3e7950769874c35725a2cae1fc8b
ICS-Brandon <wilson.brandonj15@gmail.com>
2021-03-23 14:06:21 -0400

    Added Employee functionality to transform a booking to a rental

--- Employee.java
+++ Employee.java
@@ -11,6 +11,10 @@ public class Employee {
     private Connection dbConn;
     private int e_Sin_Number, hotelId;
 
+    private static final String INSERT_RENT_SQL = "INSERT INTO public.rental" +
+            "  (rental_id,room_id,occupant_total,start_date,end_date,payment_id,is_cancelled,c_sin_number) VALUES " +
+            " (?,?,?,?,?,?,?,?);";
+
     public Employee(int eSin, int hotId){
         try{
             dbConn = DriverManager.getConnection("jdbc:postgresql://web0.site.uottawa.ca:15432/group_a07_g25","","");
@@ -77,6 +81,48 @@ public class Employee {
         return null;
     }
 
+    public void rentRoom(int bookingId, int cSin){
+
+        String getRoom = "SELECT * FROM public.booking WHERE booking_id = "+String.valueOf(bookingId)+" AND c_sin_nuber = "+cSin;
+
+        try{
+
+            PreparedStatement getBooking = dbConn.prepareStatement(getRoom);
+            ResultSet validBooking = getBooking.executeQuery();
+
+            if(validBooking.next()){
+                PreparedStatement createRental = dbConn.prepareStatement(INSERT_RENT_SQL);
+                createRental.setInt(1,getMaxRentId());
+                createRental.setInt(2,validBooking.getInt((2)));
+                createRental.setInt(3,validBooking.getInt((3)));
+                createRental.setDate(4,validBooking.getDate(4));
+                createRental.setDate(5,validBooking.getDate(5));
+                createRental.setInt(6,validBooking.getInt(6));
+                createRental.setBoolean(7,validBooking.getBoolean((7)));
+                createRental.setInt(8,cSin);
+
+                deleteBooking(bookingId,cSin);
+            }
+
+
+        } catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
+
+    public void deleteBooking(int bookingId, int cSin){
+
+        String SQL = "DELETE FROM public.booking WHERE booking_id = "+String.valueOf(bookingId)+" AND c_sin_number = "+String.valueOf(cSin);
+
+        try{
+            PreparedStatement pst = dbConn.prepareStatement(SQL);
+            pst.executeQuery();
+        } catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
 
 
     public String boolToString(boolean bool){
@@ -85,5 +131,26 @@ public class Employee {
 
     public String intToViewType(int type){
         return type == 1 ? "Sea" : "mountain";
+    }
+
+    public int getMaxRentId(){
+        String SQL = "SELECT MAX(rental_id) FROM public.rental";
+
+        try{
+
+            PreparedStatement pst = dbConn.prepareStatement(SQL);
+            ResultSet rs = pst.executeQuery();
+            if(rs.next()){
+                return rs.getInt(1)+1;
+            }
+            else{
+                return 1;
+            }
+
+        } catch(Exception e){
+            System.out.println(e);
+        }
+
+        return 1;
     }
 }
